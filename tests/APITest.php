5ca0bef68fc01bc41883a88c916e6d5d107a8c03
Nick H <nick@little-apps.com>
2015-08-25 23:58:45 -0600

    calls APITest::start_app() before each test and APITest::stop_app() after each test

--- tests/APITest.php
+++ tests/APITest.php
@@ -22,12 +22,8 @@ class APITest extends LSSTestCase {
 	public function tearDown() {
 		parent::tearDown();
 	}
-	
+
 	private function start_app() {
-		
-	}
-	
-	public function testStartApp() {
 		$data_generator = RandomDataGenerator::getInstance();
 		
 		$this->unique_id = $data_generator->unique_id();
@@ -68,86 +64,89 @@ class APITest extends LSSTestCase {
 		$this->assertEquals(1, $ret_start);
 	}
 	
-	/**
-	 * @depends testStartApp
-	 */
+	private function stop_app() {
+		$this->assertEquals( 1, API::getInstance()->stop_app( $this->timestamp + 3600 , $this->session_id ) );
+	}
+	
 	public function testEvent() {
-		$this->testStartApp();
+		$this->start_app();
 		
 		$event_info = RandomDataGenerator::getInstance()->event();
 		
 		$this->assertEquals( 1, API::getInstance()->event( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $event_info->category, $event_info->name ) );
 		
-		$this->testStopApp();
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testEvent
-	 */
 	public function testEventPeriod() {
+		$this->start_app();
+		
 		$event_period_info = RandomDataGenerator::getInstance()->event_period();
 		
 		$this->assertEquals( 1, API::getInstance()->event_value( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $event_period_info->category, $event_period_info->name, $event_period_info->duration, $event_period_info->completed ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testEventPeriod
-	 */
 	public function testEventValue() {
+		$this->start_app();
+		
 		$event_value_info = RandomDataGenerator::getInstance()->event_value();
 		
 		$this->assertEquals( 1, API::getInstance()->event_value( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $event_value_info->category, $event_value_info->name, $event_value_info->value ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testEventValue
-	 */
 	public function testLog() {
+		$this->start_app();
+		
 		$log_message = RandomDataGenerator::getInstance()->log_message();
 		
 		$this->assertEquals( 1, API::getInstance()->log( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $log_message ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testLog
-	 */
 	public function testCustomData() {
+		$this->start_app();
+		
 		$custom_data = RandomDataGenerator::getInstance()->custom_data();
 		
 		$this->assertEquals( 1, API::getInstance()->custom_data( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $custom_data->name, $custom_data->value ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testCustomData
-	 */
 	public function testException() {
+		$this->start_app();
+		
 		$exception_info = RandomDataGenerator::getInstance()->exception();
 		
 		$this->assertEquals( 1, API::getInstance()->exception( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $exception_info->message, $exception_info->stack_trace, $exception_info->source, $exception_info->target_site ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testException
-	 */
 	public function testInstall() {
+		$this->start_app();
+		
 		$app_ver = RandomDataGenerator::getInstance()->app_ver();
 		
 		$this->assertEquals( 1, API::getInstance()->exception( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $this->app_id, $app_ver ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testInstall
-	 */
 	public function testUninstall() {
+		$this->start_app();
+		
 		$app_ver = RandomDataGenerator::getInstance()->app_ver();
 		
 		$this->assertEquals( 1, API::getInstance()->exception( RandomDataGenerator::getInstance()->event_timestamp( $this->timestamp ), $this->session_id, $this->app_id, $app_ver ) );
+		
+		$this->stop_app();
 	}
 	
-	/**
-	 * @depends testUninstall
-	 */
-	public function testStopApp() {
-		$this->assertEquals( 1, API::getInstance()->stop_app( $this->timestamp + 3600 , $this->session_id ) );
-	}
+	
 }
