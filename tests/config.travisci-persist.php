f45086b11dbf6afa411bf6a187a5cf722766a1e0
Nick H <nick@little-apps.com>
2015-11-04 19:21:37 -0700

    added different config files for travis CI

---
+++ tests/config.travisci-persist.php
@@ -0,0 +1,26 @@
+<?php
+
+// This config is used with Travis CI
+
+if ( !defined( 'ROOTDIR' ) )
+    define( 'ROOTDIR', realpath( dirname( __FILE__ ) . '/../' ) );
+
+return array(
+    'site' => array(
+    	'url' => 'http://localhost/',
+    	'path' => ROOTDIR,
+    	'geoip_path' => ROOTDIR . '/geoipdb/GeoIP.dat',
+    	'geoipv6_path' => ROOTDIR . '/geoipdb/GeoIPV6.dat',
+    	'debug' => false,
+    	'csrf' => true,
+    	'header_ip_address' => true
+    ),
+    'mysql' => array(
+    	'host' => 'localhost',
+    	'user' => 'root',
+    	'pass' => '',
+    	'db' => 'test',
+    	'prefix' => 'lss_',
+    	'persistent' => true
+    )
+);
