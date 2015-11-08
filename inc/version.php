08a5bfb7d1d7b69ac07da982c4f663b0756ca59a
Nick H <nick@little-apps.com>
2015-11-08 11:47:31 -0700

    updated version information

--- inc/version.php
+++ inc/version.php
@@ -1,6 +1,6 @@
 <?php
 if ( !defined( 'VERSION' ) )
-    define( 'VERSION', '0.2' );
+    define( 'VERSION', '0.2.1' );
 
 if ( !defined( 'DB_VERSION' ) )
     define( 'DB_VERSION', 2 );
@@ -9,8 +9,8 @@ if ( !defined( 'HIGHCHARTS_VERSION' ) )
     define( 'HIGHCHARTS_VERSION', '3.0.4' );
 
 if ( !defined( 'MIN_PHP_VERSION' ) )
-    define( 'MIN_PHP_VERSION', '5.0.0' );
+    define( 'MIN_PHP_VERSION', '5.3.7' );
 
 if ( !defined( 'MIN_MYSQL_VERSION' ) )
-    define( 'MIN_MYSQL_VERSION', '5.0' );
+    define( 'MIN_MYSQL_VERSION', '5.5' );
 
