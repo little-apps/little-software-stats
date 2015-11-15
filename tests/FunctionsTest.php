cc171b4162256c12fb3e14596db519b19a0e4706
Nick H <nick@little-apps.com>
2015-11-14 20:16:28 -0700

    added PHPUnit tests for functions.php

---
+++ tests/FunctionsTest.php
@@ -0,0 +1,92 @@
+<?php
+require_once( dirname( __FILE__ ) . '/LSSTestCase.php' );
+
+class FunctionsTest extends LSSTestCase {
+	public function __construct() {
+		parent::__construct();
+		
+		if ( !is_callable( 'curl_init' ) )
+			throw new Exception( 'The cURL PHP extension must be installed' );
+	}
+
+	public function setUp() {
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+	
+	public function testTrEmpty() {
+		$this->assertEmpty(tr(''));
+	}
+	
+	public function testTrReturn() {
+		$text = 'Hello World!';
+		$this->assertSame(tr($text), gettext($text));
+	}
+	
+	public function testTrOutput() {
+		$text = 'Hello World!';
+		
+		$this->expectOutputString(gettext($text));
+		
+		_e($text);
+	}
+	
+	public function testRedirectInvalidUrl() {
+		$url = 'hxxp://www.google.com';
+		
+		$this->assertFalse(redirect($url));
+	}
+	
+	public function testRedirectInvalidStatusCode() {
+		mt_srand();
+		
+		$url = 'http://www.google.com';
+		
+		$this->assertFalse(redirect($url, mt_rand(0, 299)));
+		$this->assertFalse(redirect($url, mt_rand(309, 600)));
+	}
+	
+	/**
+	* @runInSeparateProcess
+	*/
+	public function testRedirectValidHeader() {
+		$this->assertFalse(headers_sent());
+		
+		mt_srand();
+		
+		$url = 'http://www.google.com';
+		$status_code = mt_rand(300, 308);
+		
+		$this->assertTrue(redirect($url, $status_code));
+		
+		if (function_exists('http_response_code')) {
+			$this->assertEquals(http_response_code(), $status_code);
+		}
+		
+		$this->assertContains('Location: '.$url, headers_list());
+	}
+	
+	public function testRedirectValidOutput() {
+		$this->assertTrue(headers_sent());
+		
+		mt_srand();
+		
+		$url = 'http://www.google.com';
+		$status_code = mt_rand(300, 308);
+		
+		$expected_output = 
+			'<script type="text/javascript">' . 
+            'window.location.href="'.htmlspecialchars($url).'";' . 
+            '</script>' . 
+            '<noscript>' . 
+            '<meta http-equiv="refresh" content="0;url='.htmlspecialchars($url).'" />' . 
+            '</noscript>';
+		
+		$this->expectOutputString($expected_output);
+		
+		$this->assertTrue(redirect($url, $status_code));
+	}
+}
